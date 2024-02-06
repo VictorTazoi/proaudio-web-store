07570bb87893b4768c25bbf6069c1e5d51cc702a
VictorTazoi <victortazoi@hotmail.com>
2024-02-05 21:52:16 -0300

    Finalizado sistema de adição de itens ao carrinho e calculo de valor total da compra

--- js/menu.js
+++ js/menu.js
@@ -32,73 +32,73 @@ function onClickCarrinho(){
 const produtos = [
     {
         id: 0,
-        name: 'JBL',
-        description: 'A resume about the product',
+        name: 'EchoGrove Serenity',
+        description: 'Offers exceptional comfort and balanced sound quality, providing moments of tranquility, whether at home or on the go.',
         amount: 0,
-        price: '99.9',
+        price: '149.99',
         image: 'midia/headphone-03.jpg',
         available: 29
     },
     {
         id: 1,
-        name: 'Product 02',
-        description: 'A resume about the product',
+        name: 'AcousticAura Pro',
+        description: 'Focused on premium sound quality, it offers a perfect balance between powerful bass and crystal-clear highs, using advanced technology and high-quality materials.',
         amount: 0,
-        price: '199.9',
+        price: '179.99',
         image: 'midia/headphone-03.jpg',
         available: 2
     },
     {
         id: 2,
-        name: 'Product 03',
-        description: 'A resume about the product',
+        name: 'TempoTunes Bliss',
+        description: 'Affordable, lightweight, and comfortable headphones, providing clear and balanced sound for those seeking quality without compromising the budget.',
         amount: 0,
-        price: '499.9',
+        price: '129.99',
         image: 'midia/headphone-03.jpg',
         available: 29
     },
     {
         id: 3,
-        name: 'Product 04',
-        description: 'A resume about the product',
+        name: 'HarmonyHarbor Luxe',
+        description: 'Luxurious in design and performance, these premium headphones offer high-quality finish, advanced connectivity, and an exceptional frequency response.',
         amount: 0,
-        price: '499.9',
+        price: '199.99',
         image: 'midia/headphone-03.jpg',
         available: 29
     },
     {
         id: 4,
-        name: 'Product 05',
-        description: 'A resume about the product',
+        name: 'BassBeat Forge',
+        description: 'Designed for deep bass lovers, the BassBeat Forge offers an immersive audio experience with an emphasis on deeper sounds, being an affordable option.',
         amount: 0,
-        price: '499.9',
+        price: '89.99',
         image: 'midia/headphone-03.jpg',
         available: 29
     },
     {
         id: 5,
-        name: 'Product 06',
-        description: 'A resume about the product',
+        name: 'InfinitySound Zenith',
+        description: 'Designed for discerning audio enthusiasts, InfinitySound Zenith offers exceptional sound reproduction across all frequencies, providing an unparalleled audio experience.',
         amount: 0,
-        price: '499.9',
+        price: '169.99',
         image: 'midia/headphone-03.jpg',
         available: 29
     },
     {
         id: 6,
-        name: 'Product 07',
-        description: 'A resume about the product',
+        name: 'PulseSync Horizon',
+        description: 'Provides seamless synchronization between audio and ergonomic design for an immersive musical experience, ideal for those seeking comfort and sound quality.',
         amount: 0,
-        price: '499.9',
+        price: '139.99',
         image: 'midia/headphone-03.jpg',
         available: 29
     },
     {
         id: 7,
-        name: 'Product 08',
-        description: 'A resume about the product',
+        name: 'ZenSonic Precision',
+        description: 'Focused on sonic precision, ZenSonic Precision offers faithful and clear audio reproduction, being the choice for those looking for a precise auditory experience.',
         amount: 0,
-        price: '499.9',
+        price: '159.99',
         image: 'midia/headphone-03.jpg',
         available: 29
     }
@@ -131,63 +131,76 @@ listagemProdutos();
 
 
 // CARRINHO
+
+//Enquanto o carrinho estiver vazio, exibe a mensagem 'Carrinho Vazio'
 var showCart = document.getElementById('showCart');
 showCart.innerHTML = 'Empty Cart';
+
+//Enquanto o carrinho estiver vazio, exibe uma imagem 'Lupa'
 var conteinerCarrinho = document.getElementById('conteinerCarrinho');
 conteinerCarrinho.innerHTML = "<img src='../midia/icons/icon-empty-100.png'>"
 
 function atualizarCarrinho(){
-    var conteinerCarrinho = document.getElementById('conteinerCarrinho');
+    //Após o carrinho receber um produto, troca a mensagem anterior pela mensagem 'Carrinho'
     var showCart = document.getElementById('showCart');
     showCart.innerHTML = 'Cart';
+    //Após o carringo receber um produto, apaga a imagem 'lupa'
+    var conteinerCarrinho = document.getElementById('conteinerCarrinho');
     conteinerCarrinho.innerHTML = '';
+
+    var totalCarrinho = 0;//Variável do valor total adicionado ao carrinho
+
     produtos.map((val)=>{
-        if(val.amount > 0 && val.amount <= val.available){
-            val.available - 1;
-            document.getElementsByClassName('onCartProduto')[val.id].innerText = `Cart: ${val.amount}`
+
+        if(val.amount > 0 && val.amount <= val.available){ //CASO O USUÁRIO ADICIONE UMA QUANTIDADE QUE AINDA ESTEJA DENTRO DO LIMITE DO ESTOQUE:
+
+            val.available - 1; //Subtrai 1 unidade do estoque
+
+            document.getElementsByClassName('onCartProduto')[val.id].innerText = `Cart: ${val.amount}` //Retorna no card da listagem um valor correspondente á quantidade selecionada
+
             conteinerCarrinho.innerHTML += `
             <div class="card mb-3 cardCarrinho">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img class="imagemCarrinho" src="`+val.image+`"/>
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                        <p class="tituloCarrinho">`+val.name+`</p>
-                        <p class="precoCarrinho">Price: <strong>`+tipoMoeda+` `+val.price+`</strong></p>
-                        <p class="quantidadeCarrinho">Amount: <strong>`+val.amount+`un</strong></p>
-                        <p class="quantidadeCarrinho">Total: <strong>`+tipoMoeda+` `+(val.price*val.amount).toFixed(2)+`</strong></p>
-                        </div>
-                    </div>
+                <div class="card-body">
+                    <div class="tituloCarrinho">`+val.name+`</div>
+                    <div class="precoCarrinho">Price: <strong>`+tipoMoeda+` `+val.price+` | `+val.amount+`un</strong></p>
+                    <div class="quantidadeCarrinho">Total: <strong>`+tipoMoeda+` `+(val.price*val.amount).toFixed(2)+`</strong></div>
                 </div>
-            </div>
-        `;
-        }else if(val.amount >= val.available){ //CASO O USUÁRIO SELECIONE A QUANTIDADE MÁXIMA DISPONÍVEL NO ESTOQUE:
+            </div>`;
+        totalCarrinho += Number(val.price*val.amount)//Calcula o preço * quantidade e atribui á variável totalCarrinho
+        }
+        
+        else if(val.amount >= val.available){ //CASO O USUÁRIO SELECIONE A QUANTIDADE MÁXIMA DISPONÍVEL NO ESTOQUE:
+
             val.amount = val.available; //Quantidade igual a estoque
 
             //Retorno ao usuário de Limite excedido
-            let onCart = document.getElementsByClassName('onCartProduto')[val.id];
-            onCart.style.color = 'red';
-            onCart.innerText = `Cart: ${val.amount} | No more units available!`
+            let onCart = document.getElementsByClassName('onCartProduto')[val.id]; //Elemento ID onCartProduto (Feedback de quantidade)
+            onCart.style.color = 'red'; //Ao exceder o limite do estoque, aplicar fonte vermelha
+            onCart.innerText = `Cart: ${val.amount} | No more units available!` //Ao exceder o limite do estoque, retornar mensagem
 
             conteinerCarrinho.innerHTML += `
             <div class="card mb-3 cardCarrinho">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img class="imagemCarrinho" src="`+val.image+`"/>
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                        <p class="tituloCarrinho">`+val.name+`</p>
-                        <p class="precoCarrinho">Price: <strong>`+val.price+`</strong></p>
-                        <p class="quantidadeCarrinho">Amount: <strong>`+val.amount+`un</strong></p>
-                        <p class="quantidadeCarrinho">Total: <strong>`+tipoMoeda+` `+(val.price*val.amount).toFixed(2)+`</strong></p>
-                        </div>
-                    </div>
+                <div class="card-body">
+                    <div class="tituloCarrinho">`+val.name+`</div>
+                    <div class="precoCarrinho">Price: <strong>`+tipoMoeda+` `+val.price+` | `+val.amount+`un</strong></p>
+                    <div class="quantidadeCarrinho">Total: <strong>`+tipoMoeda+` `+(val.price*val.amount).toFixed(2)+`</strong></div>
                 </div>
-            </div>`
+            </div>`;
+            totalCarrinho += Number(val.price*val.amount); //Calcula o preço * quantidade e atribui á variável totalCarrinho
         }
     })
+
+    
+    //Calculo de valor de frete
+    var valorFrete = 40.00; //Valor estatico de frete
+    var freteResumo = document.getElementById('totalFrete'); // Coleta o elemento de ID 'totalFrete' para atribuição do valor do frete
+    freteResumo.innerText = ' ' + tipoMoeda + valorFrete;
+
+    totalCarrinho += valorFrete
+    
+    //Calculo de valor total do carrinho
+    var totalResumo = document.getElementById('totalResumo'); //Coleta o elemento de ID 'totalResumo' para futura atribuição da variável 'valorCarrinho'
+    totalResumo.innerText = ' ' + tipoMoeda + Number(totalCarrinho.toFixed(2));
 }
 
 var links = document.getElementsByClassName('carrinhoProduto');
